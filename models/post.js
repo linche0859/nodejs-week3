2038162cefee6c9a109e4eb2476fbda412b9c96f
che.lin <aa10327tw@gmail.com>
2022-04-23 16:17:14 +0800

    feat: 新增 post model

---
+++ models/post.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema(
+  {
+    userName: { type: String, required: [true, '發文者名稱必填'] },
+    userPhoto: { type: String, required: [true, '發文者照片必填'] },
+    liked: { type: Boolean, default: false },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+module.exports = mongoose.model('Post', schema);
